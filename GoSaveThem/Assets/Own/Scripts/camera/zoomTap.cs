235879670d5cf0bb158f2319b84cc87947586358
Unknown <ericl3@kth.se>
2019-03-18 22:26:55 +0100

    doubleTap

---
+++ GoSaveThem/Assets/Own/Scripts/camera/zoomTap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoomTap : MonoBehaviour
+{
+    int tapCount;
+    public float MaxDubbleTapTime;
+    float NewTime;
+ 
+    void Start () {
+         tapCount = 0;
+         MaxDubbleTapTime = 0.12f;
+    }
+ 
+void Update () {
+    
+    if (Input.touchCount == 1) {
+        Touch touch = Input.GetTouch (0);
+        Vector2 lastpos = touch.deltaPosition;
+        //Debug.Log(lastpos.magnitude); 
+
+                
+        if ((touch.phase == TouchPhase.Ended)) {
+            tapCount += 1;
+        }
+    
+        if (tapCount == 1) {
+            NewTime = Time.time + MaxDubbleTapTime;
+
+        }else if(tapCount == 2 && Time.time <= NewTime){
+
+            Debug.Log("Dubble tap"); 
+            tapCount = 0;
+        }
+        Debug.Log("tap: "+tapCount); 
+    }
+    if (Time.time > NewTime) {
+        tapCount = 0; 
+    }
+}
+}
+
