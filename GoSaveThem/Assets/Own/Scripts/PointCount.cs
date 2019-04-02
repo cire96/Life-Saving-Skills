9a006a8cbc5dc8b236bfb5da15e593c542da18db
Unknown <ericl3@kth.se>
2019-04-02 09:31:42 +0200

    stage point script

---
+++ GoSaveThem/Assets/Own/Scripts/PointCount.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCount : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Points;
+    public GameObject[] victims;
+    private Parameters victimParam;
+    void Start()
+    {   
+
+        //victimGroup = GameObject.Find("VictumGroup");
+        Points=0;
+        victims = GameObject.FindGameObjectsWithTag("Victim");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countPoints();
+    }
+
+    void countPoints(){
+
+        victimParam=victims[0].GetComponent<Parameters>();
+        Debug.Log(victimParam.Bfreq);
+    }
+}
