9f714fa01e0ec88ce96bf71a4d849c5224428853
Unknown <ericl3@kth.se>
2019-04-17 23:20:29 +0200

    Poängsystem print out

--- GoSaveThem/Assets/Own/Scripts/PointCount.cs
+++ GoSaveThem/Assets/Own/Scripts/PointCount.cs
@@ -9,9 +9,23 @@ public class PointCount : MonoBehaviour
     public int Points;
     public GameObject[] victims;
     public Parameters victimParam;
+    public GameObject HUD;
+    Countdown countdownScript;
+    int timeFactor = 2;
+    int correctTriagePoint;
+    int unSetTriagePenelty;
+    int incorrectTriagePenelty;
+
     void Start()
     {   
-        Points=0;        
+        Points=0;
+        correctTriagePoint = 100 * timeFactor;
+        unSetTriagePenelty = 75 * timeFactor;
+        incorrectTriagePenelty = 25*timeFactor;
+
+
+        GameObject countdown = HUD.transform.Find("countdownTxt").gameObject;
+        countdownScript = countdown.GetComponent<Countdown>();
     }
 
     // Update is called once per frame
@@ -21,11 +35,27 @@ public class PointCount : MonoBehaviour
     }
 
     public void countPoints(){
+        Points += timeFactor*countdownScript.getTimeLeft();
         victims = GameObject.FindGameObjectsWithTag("Victim");
         foreach (GameObject victim in victims){
             victimParam=victim.GetComponent<Parameters>();
             Dictionary<string, object> ParamDic = victimParam.getParamHash();
-            Debug.Log(ParamDic["prio"]+" : "+ParamDic["SetPrio"]);
+            if ((string)ParamDic["SetPrio"] == "unset")
+            {
+                Points -= unSetTriagePenelty;
+            }
+            else if((string)ParamDic["SetPrio"]==ParamDic["prio"].ToString().ToLower()){
+                Debug.Log("Correct");
+                Points += correctTriagePoint; 
+            }
+            else if ((string)ParamDic["SetPrio"] != ParamDic["prio"].ToString().ToLower())
+            {
+                Debug.Log("incorrect");
+                Points -= incorrectTriagePenelty;
+            }
+            //Debug.Log(ParamDic["prio"]+" : "+ParamDic["SetPrio"]);
         }
+        Debug.Log("Points: " + Points);
+        Points = 0;
     }
 }
